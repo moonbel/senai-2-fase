7945e03ddcc8d036641899979367dd05ecda7f9b
HIZABELE ROBERTA DE SOUZA <hizabele_souza@estudante.sesisenai.org.br>
2026-08-12 21:57:23 -0300

    segunda fase

---
+++ 01.scripts/main.js
@@ -0,0 +1,5 @@
+import exemplosVariavel from "./01.scripts/variaveis.js";
+
+
+const btnVariavel = document.getElementById("btnVariavel");
+btnVariavel.addEventListener("click".exemplosVariavel);
---
+++ 01.scripts/variaveis.js
@@ -0,0 +1,5 @@
+function exemplosVariavel(){
+console.log("Olá, mundo origen variaveis.js");
+
+}
+export default exemplosVariavel;
